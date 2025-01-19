31772f5135b5913aefe25d3d162d8e993c26c13e
Matis André <45696697+tismatis@users.noreply.github.com>
2025-01-19 04:27:31 +0100

    Separated all Console methods declaration into another static class for better reading

---
+++ AutoBackupFiles/Console.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AutoBackupFiles
+{
+    internal static class Console
+    {
+        private static bool _k;
+#region CMD Compatibility Mode
+        private static bool _cmdMode;
+
+        public static void Initialize()
+        {
+            _cmdMode = (Environment.GetEnvironmentVariable("TERM_PROGRAM") ??
+                        Environment.GetEnvironmentVariable("ComSpec") ??
+                        "Unknown").Contains("cmd.exe");
+        }
+        public static void ForceCmdMode(bool force) => _cmdMode = force;
+#endregion CMD Compatibility Mode
+#region System.Console Wrappers
+        public static void Write(string message, bool disableTimezone = false) => WriteFormattedMessage($"{(!disableTimezone ? $"{DateTime.Now} &d&l>>&r " : "")}{message}");
+        public static System.ConsoleKeyInfo ReadKey() => System.Console.ReadKey();
+        public static void SetCursorPosition(int left, int top) => System.Console.SetCursorPosition(left, top);
+        public static int CursorTop => System.Console.CursorTop;
+        public static int CursorLeft => System.Console.CursorLeft;
+#endregion System.Console Wrappers
+#region Formatting
+        private static void WriteFormattedMessage(string input)
+        {
+            string total = "";
+
+            var message = input.Replace("&", "§");
+            var regex = new Regex(@"§.");
+            var matches = regex.Matches(message);
+            int lastIndex = 0;
+
+            foreach (Match match in matches)
+            {
+                System.Console.Write(_k ? GenerateCensoredShit(match.Index - lastIndex) : message.Substring(lastIndex, match.Index - lastIndex));
+                total += message.Substring(lastIndex, match.Index - lastIndex);
+                
+                lastIndex = match.Index + match.Length;
+
+                GetConsoleColor(match.Value[1]);
+            }
+
+            System.Console.WriteLine(message.Substring(lastIndex));
+            System.Console.ResetColor();
+            if(!_cmdMode)
+                System.Console.Write("\x1b[22m\x1b[23m\x1b[29m\x1b[24m");
+            _k = false;
+
+            File.AppendAllText("logs.txt", total + message.Substring(lastIndex) + "\n");
+        }
+
+        private static string GenerateCensoredShit(int length)
+        {
+            var random = new Random();
+            const string chars = "█";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private static void GetConsoleColor(char colorCode)
+        {
+            if(_cmdMode)
+                switch (colorCode)
+                {
+                    case 'l': // Bold
+                        return;
+                    case 'i': // Italic
+                        return;
+                    case 's': // Strikethrough
+                        return;
+                    case 'u': // Underline
+                        return;
+                    case 'r': // Reset
+                        System.Console.ResetColor();
+                        _k = false;
+                        return;
+                    default:
+                        break;
+                }
+            
+            switch(colorCode)
+            {
+                case '0':
+                    System.Console.ForegroundColor = ConsoleColor.Black;
+                    return;
+                case '1':
+                    System.Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    return;
+                case '2':
+                    System.Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    return;
+                case '3':
+                    System.Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    return;
+                case '4':
+                    System.Console.ForegroundColor = ConsoleColor.DarkRed;
+                    return;
+                case '5':
+                    System.Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    return;
+                case '6':
+                    System.Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    return;
+                case '7':
+                    System.Console.ForegroundColor = ConsoleColor.Gray;
+                    return;
+                case '8':
+                    System.Console.ForegroundColor = ConsoleColor.DarkGray;
+                    return;
+                case '9':
+                    System.Console.ForegroundColor = ConsoleColor.Blue;
+                    return;
+                case 'a':
+                    System.Console.ForegroundColor = ConsoleColor.Green;
+                    return;
+                case 'b':
+                    System.Console.ForegroundColor = ConsoleColor.Cyan;
+                    return;
+                case 'c':
+                    System.Console.ForegroundColor = ConsoleColor.Red;
+                    return;
+                case 'd':
+                    System.Console.ForegroundColor = ConsoleColor.Magenta;
+                    return;
+                case 'e':
+                    System.Console.ForegroundColor = ConsoleColor.Yellow;
+                    return;
+                case 'f':
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                    return;
+                case 'l': // Bold
+                    System.Console.Write("\x1b[1m");
+                    break;
+                case 'i': // Italic
+                    System.Console.Write("\x1b[3m");
+                    break;
+                case 's': // Strikethrough
+                    System.Console.Write("\x1b[9m");
+                    break;
+                case 'u': // Underline
+                    System.Console.Write("\x1b[4m");
+                    break;
+                case 'r': // Reset
+                    System.Console.ResetColor();
+                    System.Console.Write("\x1b[22m\x1b[23m\x1b[29m\x1b[24m"); // Reset bold, italic, strikethrough, underline
+                    _k = false;
+                    break;
+                case 'k':
+                    _k = true;
+                    break;
+                default:
+                    throw new Exception($"{colorCode} is not a valid code.");
+            }
+        }
+#endregion Formatting
+    }
+}
--- AutoBackupFiles/Program.cs
+++ AutoBackupFiles/Program.cs
@@ -6,9 +6,8 @@ using System.Text.RegularExpressions;
 
 namespace AutoBackupFiles
 {
-    internal class Program
+    internal static class Program
     {
-        private static bool _cmdMode;
         public static void Main(string[] args)
         {
             if(File.Exists("logs.txt"))
@@ -16,14 +15,9 @@ namespace AutoBackupFiles
             
             File.WriteAllText("logs.txt", "");
             
-            string terminalName = Environment.GetEnvironmentVariable("TERM_PROGRAM") ?? 
-                                  Environment.GetEnvironmentVariable("ComSpec") ?? 
-                                  "Unknown";
-            _cmdMode = terminalName.Contains("cmd.exe");
-            
             try
             {
-                WriteConsole("&5&l" + (@"
+                Console.Write("&5&l" + (@"
  ________  ________  ________  ___  __    ___  ___  ________   
 |\   __  \|\   __  \|\   ____\|\  \|\  \ |\  \|\  \|\   __  \  
 \ \  \|\ /\ \  \|\  \ \  \___|\ \  \/  /|\ \  \\\  \ \  \|\  \ 
@@ -44,12 +38,12 @@ namespace AutoBackupFiles
                     switch (args[1])
                     {
                         case "--force-special-chars":
-                            _cmdMode = false;
-                            WriteConsole("&aForcing special chars!");
+                            Console.ForceCmdMode(false);
+                            Console.Write("&aForcing special chars!");
                             break;
                         case "--force-normal-chars":
-                            _cmdMode = true;
-                            WriteConsole("&cForcing ban special chars!");
+                            Console.ForceCmdMode(true);
+                            Console.Write("&cForcing ban special chars!");
                             break;
                         default:
                             throw new Exception("&4Invalid argument provided!&r");
@@ -58,18 +52,18 @@ namespace AutoBackupFiles
                 if(!File.Exists(args[0]))
                     throw new Exception("&4The file you provided &ldoes not exist&r!");
 
-                WriteConsole("&7Reading/Basic Parsing CSV file...");
+                Console.Write("&7Reading/Basic Parsing CSV file...");
                 
                 string[][] csv = CSVParser.Parse(args[0]);
                 
                 Console.SetCursorPosition(0, Console.CursorTop - 1);
-                WriteConsole("&aReading and Basic Parsing complete!");
+                Console.Write("&aReading and Basic Parsing complete!");
                 
                 string dateformat = "dd-mm-yyyy_HH-mm-ss";
                 string destination = "";
                 Dictionary<string, ObjectRestore> objects = new Dictionary<string, ObjectRestore>();
 
-                WriteConsole("&7Starting listing...");
+                Console.Write("&7Starting listing...");
                 foreach (string[] list in csv)
                 {
                     if(list[0].StartsWith(" ") && list.Length == 1 || list[0] == "" || list[0].StartsWith("#") && list.Length == 1)
@@ -107,19 +101,19 @@ namespace AutoBackupFiles
                 destination = destination.Replace("%DATE%", DateTime.Now.ToString(dateformat));
 
                 Console.SetCursorPosition(0, Console.CursorTop - 1);
-                WriteConsole("&aListing &lcomplete!            ");
+                Console.Write("&aListing &lcomplete!            ");
 
                 if(destination == "")
                     throw new Exception("No destination has been provided!");
 
-                WriteConsole("&7Starting backup...");
+                Console.Write("&7Starting backup...");
 
                 foreach(KeyValuePair<string, ObjectRestore> obj in objects)
                 {
-                    WriteConsole($"&7Copying {obj.Key}...");
+                    Console.Write($"&7Copying {obj.Key}...");
                     foreach(string[] path in obj.Value.Paths)
                     {
-                        WriteConsole($"&7Copying {path[1]}&r&7!");
+                        Console.Write($"&7Copying {path[1]}&r&7!");
                         switch(path[0])
                         {
                             case "folder":
@@ -132,20 +126,20 @@ namespace AutoBackupFiles
                                 throw new Exception("Parsing error! An non reconized line has been readed.");
                         }
                         Console.SetCursorPosition(0, Console.CursorTop - 1);
-                        WriteConsole($"&aCopied successfuly {path[1]}&r&a!");
+                        Console.Write($"&aCopied successfuly {path[1]}&r&a!");
                     }
-                    WriteConsole($"&7Writing list of backuped files for {obj.Key}...");
+                    Console.Write($"&7Writing list of backuped files for {obj.Key}...");
                     File.WriteAllText(Path.Combine(Path.Combine($"{destination}", obj.Key), "list_of_backuped_files.txt"), obj.Value.ToString());
                     Console.SetCursorPosition(0, Console.CursorTop - 1);
-                    WriteConsole($"&aCopied successfuly {obj.Key}&r&a!                                      ");
+                    Console.Write($"&aCopied successfuly {obj.Key}&r&a!                                      ");
                 }
-                WriteConsole("&aBackup done &lsuccessfully&r&a!");
+                Console.Write("&aBackup done &lsuccessfully&r&a!");
             }catch(Exception ex)
             {
-                WriteConsole($"&4&lOno, an error has occured: &r&4{ex.Message}\n{ex.StackTrace}");
+                Console.Write($"&4&lOno, an error has occured: &r&4{ex.Message}\n{ex.StackTrace}");
             }
 
-            WriteConsole("&7Press any key to &4exit&r&7...");
+            Console.Write("&7Press any key to &4exit&r&7...");
             Console.ReadKey();
         }
         
@@ -176,144 +170,8 @@ namespace AutoBackupFiles
             {
                 Directory.CreateDirectory(destDir);
             }
+
             File.Copy(sourceFile, destFile, true);
-        }
-        
-        private static void WriteConsole(string message, bool disableTimezone = false) => WriteConsoleSpecial($"{(!disableTimezone ? $"{DateTime.Now} &d&l>>&r " : "")}{message}");
-
-        private static bool _k = false;
-        private static void WriteConsoleSpecial(string input)
-        {
-            string total = "";
-
-            var message = input.Replace("&", "§");
-            var regex = new Regex(@"§.");
-            var matches = regex.Matches(message);
-            int lastIndex = 0;
-
-            foreach (Match match in matches)
-            {
-                // Print the text before the color code
-                Console.Write(_k ? GenerateCensoredShit(match.Index - lastIndex) : message.Substring(lastIndex, match.Index - lastIndex));
-                total += message.Substring(lastIndex, match.Index - lastIndex);
-                
-                lastIndex = match.Index + match.Length;
-
-                // Change the console color based on the color code
-                GetConsoleColor(match.Value[1]);
-            }
-
-            // Print the remaining part of the message
-            Console.WriteLine(message.Substring(lastIndex));
-            Console.ResetColor();
-            if(!_cmdMode)
-                Console.Write("\x1b[22m\x1b[23m\x1b[29m\x1b[24m");
-            _k = false;
-
-            File.AppendAllText("logs.txt", total + message.Substring(lastIndex) + "\n");
-        }
-
-        private static string GenerateCensoredShit(int length)
-        {
-            var random = new Random();
-            const string chars = "█";
-            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
-        private static void GetConsoleColor(char colorCode)
-        {
-            if(_cmdMode)
-                switch (colorCode)
-                {
-                    case 'l': // Bold
-                        return;
-                    case 'i': // Italic
-                        return;
-                    case 's': // Strikethrough
-                        return;
-                    case 'u': // Underline
-                        return;
-                    case 'r': // Reset
-                        Console.ResetColor();
-                        _k = false;
-                        return;
-                    default:
-                        break;
-                }
-            
-            switch(colorCode)
-            {
-                case '0':
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    return;
-                case '1':
-                    Console.ForegroundColor = ConsoleColor.DarkBlue;
-                    return;
-                case '2':
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    return;
-                case '3':
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    return;
-                case '4':
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    return;
-                case '5':
-                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    return;
-                case '6':
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    return;
-                case '7':
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    return;
-                case '8':
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    return;
-                case '9':
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    return;
-                case 'a':
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    return;
-                case 'b':
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    return;
-                case 'c':
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    return;
-                case 'd':
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    return;
-                case 'e':
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    return;
-                case 'f':
-                    Console.ForegroundColor = ConsoleColor.White;
-                    return;
-                case 'l': // Bold
-                    Console.Write("\x1b[1m");
-                    break;
-                case 'i': // Italic
-                    Console.Write("\x1b[3m");
-                    break;
-                case 's': // Strikethrough
-                    Console.Write("\x1b[9m");
-                    break;
-                case 'u': // Underline
-                    Console.Write("\x1b[4m");
-                    break;
-                case 'r': // Reset
-                    Console.ResetColor();
-                    Console.Write("\x1b[22m\x1b[23m\x1b[29m\x1b[24m"); // Reset bold, italic, strikethrough, underline
-                    _k = false;
-                    break;
-                case 'k':
-                    _k = true;
-                    break;
-                default:
-                    throw new Exception($"{colorCode} is not a valid code.");
-            }
         }
     }
 }
