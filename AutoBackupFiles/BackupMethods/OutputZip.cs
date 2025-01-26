d25d5aa61560dcf94ddedc58152ccaac0dce9f97
Matis André <45696697+tismatis@users.noreply.github.com>
2025-01-26 18:58:50 +0100

    Refactor OutputZip

--- AutoBackupFiles/BackupMethods/OutputZip.cs
+++ AutoBackupFiles/BackupMethods/OutputZip.cs
@@ -5,9 +5,11 @@ namespace AutoBackupFiles.BackupMethods;
 
 public static class OutputZip
 {
-    public static void Backup(string tempDir, ZIPConfiguration cfg)
+    public static void Backup(string tempDir, ZIPConfiguration cfg) => Backup(tempDir, Path.Combine(cfg.Path, cfg.FileName), cfg.Name);
+    
+    public static void Backup(string tempDir, string pathAndName, string name = "")
     {
-        Console.Write($"&7Creating zip file {cfg.Name}...");
+        Console.Write($"&7Creating zip file{(String.IsNullOrEmpty(name) ? "" : $" {name}")}...");
 
         var files = Directory.GetFiles(tempDir, "*", SearchOption.AllDirectories);
         long totalSize = files.Sum(file => new FileInfo(file).Length);
@@ -16,7 +18,7 @@ public static class OutputZip
         Stopwatch stopwatch = new Stopwatch();
         
         stopwatch.Start();
-        using (var zip = ZipFile.Open($"{cfg.Path}/{cfg.FileName}", ZipArchiveMode.Create))
+        using (var zip = ZipFile.Open($"{pathAndName}", ZipArchiveMode.Create))
         {
             for (int i = 0; i < files.Length; i++)
             {
@@ -30,31 +32,6 @@ public static class OutputZip
             }
         }
         stopwatch.Stop();
-        Console.Write($"&aZip file created!");
-    }
-    
-    public static void Backup(string tempDir, string pathAndName)
-    {
-        Console.Write("&aBackup done &lsuccessfully&r&a!");
-        Console.Write("&7Creating zip file...");
-
-        var files = Directory.GetFiles(tempDir, "*", SearchOption.AllDirectories);
-        long totalSize = files.Sum(file => new FileInfo(file).Length);
-        long processedSize = 0;
-
-        using (var zip = ZipFile.Open(pathAndName, ZipArchiveMode.Create))
-        {
-            for (int i = 0; i < files.Length; i++)
-            {
-                var file = files[i];
-                var entryName = Path.GetRelativePath(tempDir, file);
-                zip.CreateEntryFromFile(file, entryName);
-                processedSize += new FileInfo(file).Length;
-                double progress = (double)processedSize / totalSize * 100;
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-                Console.Write($"&eCreating zip file... {progress:0.00}% complete! [{(processedSize / 1024.0 / 1024.0):0.00}Mo processed, {(totalSize - processedSize) / 1024.0 / 1024.0:0.00}Mo remaining]", Program.ForceSaveDownloadLog);
-            }
-        }
         Console.Write("&aZip file created!");
     }
 }
